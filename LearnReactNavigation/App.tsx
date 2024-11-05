e41097883ae5ea312f1b55efd33bb58e1841d79c
ui0426 <ul0426@naver.com>
2024-11-06 00:09:42 +0900

    navigation

---
+++ LearnReactNavigation/App.tsx
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View><Text>12223</Text></View>
+  )
+}
+
+
+export default App;
